d633e57c92e821b5446b00d157ffad6444c58f80
rechh <rechh@channing.harvard.edu>
2018-08-14 20:49:36 -0400

    final version

--- src/lab4/ShellSort.java
+++ src/lab4/ShellSort.java
@@ -1,80 +1,23 @@
 package lab4;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Arrays;
 
 public class ShellSort {
-	private static int N = 50;
-
-	public static void main(String args[]) {
-		// start recording time
-		final long startTime = System.currentTimeMillis();
-
-		// build array
-		int arr[] = new int[N];
-		int gapArr[] = {1, 10, 30, 60, 120, 360, 1080, 3240, 9720, 29160};
-
-		String inputFilePath = args[0];
-		// String outputFilePath = args[1];
-
-		FileReader fileReader = null;
-		BufferedReader bufferedReader = null;
-		// PrintWriter writer = null;
-
-		try {
-			fileReader = new FileReader(inputFilePath);
-			bufferedReader = new BufferedReader(fileReader);
-
-			// write the result to file
-			// File outFile = new File(outputFilePath);
-			// writer = new PrintWriter(outFile);
-
-			// start reading file line by line
-			String line;
-			int i = 0;
-			while ((line = bufferedReader.readLine()) != null) {
-				// each line is a number, adding it to array
-				arr[i] = Integer.parseInt(line);
-				i++;
-			}
-
-			// sorting
-			ShellSort myShellSorter = new ShellSort();
-			int[] validGaps = getValidGapArr(gapArr);			
-			
-//			for(int j = 0; j < validGaps.length; j++) {
-//				System.out.print(validGaps[j] + " ");
-//			}
-			myShellSorter.shellSort(arr, validGaps);
-//			myShellSorter.showArray(arr);
-
-			// record end time
-			final long endTime = System.currentTimeMillis();
-			System.out.println("Total execution time in milliseconds: " + (endTime - startTime));
-
-		} catch (FileNotFoundException e) {
-			System.out.println(e);
-		} catch (IOException e) {
-			System.out.println(e);
-		} finally {
-			try {
-				bufferedReader.close();
-				fileReader.close();
-				// writer.close();
-			} catch (IOException e) {
-				System.out.println(e);
-			}
-
-		}
+	private int fileSize;
+	
+	
+	public ShellSort(int fileSize) {
+		super();
+		this.fileSize = fileSize;
 	}
 
-	public static int[] getValidGapArr(int[] gapArr) {
+	/*
+	 * get valid gaps based on file size
+	 */
+	public int[] getValidGapArr(int[] gapArr) {
 		for (int i = gapArr.length - 1; i >= 0; i--) {
 			
-			if (gapArr[i -1] < N && gapArr[i] > N) {
+			if (gapArr[i -1] < fileSize && gapArr[i] > fileSize) {
 				return Arrays.copyOfRange(gapArr, 0, i - 1);
 					
 			}
@@ -90,7 +33,7 @@ public class ShellSort {
 			int gap = gapArr[gapIndex];
 			
 			if (gap > 0) {
-				System.out.println("Gap is " + gap);
+//				System.out.println("Gap is " + gap);
 
 				for (int i = gap; i < arr.length; i++) {
 					int temp = arr[i];
@@ -103,14 +46,17 @@ public class ShellSort {
 					arr[j] = temp;
 				}
 
-				showArray(arr);
+//				showArray(arr);
 			}
 		}
 
 
 	}
 
-	private void showArray(int arr[]) {
+	/*
+	 * display sorted array content
+	 */
+	public void showArray(int arr[]) {
 		System.out.println("Sorted array:");
 
 		int n = arr.length;
@@ -119,4 +65,5 @@ public class ShellSort {
 		}
 		System.out.println();
 	}
+
 }
---
+++ src/lab4/ShellSortV1.java
@@ -0,0 +1,62 @@
+package lab4;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ShellSortV1 {
+	public static final int FILE_SIZE = 50;
+	public static final int[] GAP_ARR = {1, 4, 13, 40, 121, 364, 1093, 3280, 9841, 29524};
+
+	public static void main(String args[]) {
+		// start recording time
+		final long startTime = System.currentTimeMillis();
+
+		// build array
+		int arr[] = new int[FILE_SIZE];
+		
+		String inputFilePath = args[0];
+
+		FileReader fileReader = null;
+		BufferedReader bufferedReader = null;
+
+		try {
+			fileReader = new FileReader(inputFilePath);
+			bufferedReader = new BufferedReader(fileReader);
+
+			// start reading file line by line
+			String line;
+			int i = 0;
+			while ((line = bufferedReader.readLine()) != null) {
+				// each line is a number, adding it to array
+				arr[i] = Integer.parseInt(line);
+				i++;
+			}
+
+			// sorting based on the given gap array
+			ShellSort myShellSorter = new ShellSort(50);
+			int[] validGaps = myShellSorter.getValidGapArr(GAP_ARR);			
+			myShellSorter.shellSort(arr, validGaps);
+			myShellSorter.showArray(arr);
+
+			// record end time
+			final long endTime = System.currentTimeMillis();
+			System.out.println("Total execution time in milliseconds: " + (endTime - startTime));
+
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		} catch (IOException e) {
+			System.out.println(e);
+		} finally {
+			try {
+				bufferedReader.close();
+				fileReader.close();
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+
+		}
+	}
+
+}
